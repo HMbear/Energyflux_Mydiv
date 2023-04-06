2fe9a831b33d286943557a7d5667b75c05eeda39
HMbear <99480771+HMbear@users.noreply.github.com>
2023-04-06 22:33:45 +0200

    Update Collembola.r

--- Collembola.r
+++ Collembola.r
@@ -11,16 +11,18 @@ Collembola_Final <- Collembola_Community %>% full_join(Collembola_Alllength) %>%
   filter(!is.na(Abundance)) %>% 
   left_join(
   Collembola_twice %>% select(order, suborder, family) %>% unique 
-) %>% 
+) %>% ### The aimed structure: sampling_id, year, month, plot, depth, order, suborder, family, abundance, density_m2, average bodylength (each family in sampling soil core) 
 left_join(LWbiomass %>% rename("suborder" = "Study_Group") %>% 
                   select(suborder, Measures, Functions, LC, a, b1, b2,`Function(WW-FW)`, c,d, lni0, s, E_evK) %>% mutate(b1 = as.numeric(b1))) %>% 
- ### The argument here, the function was attained by using the photography pictures
+ ### The argument here, the function was attained by using the photography pictures (LC)
   mutate(DW_ug = a*(LC*bodylength_mm)^b1) %>% mutate(FW_ug = c*DW_ug^d)   %>% 
   mutate(Biomass_mgm2 = FW_ug*Density_m2/1000) %>% 
-  mutate(Tk = ifelse(month == "May" & depth == 5,Temp_AprMay_soil5cm, ###Asing the environmental temperature based on the month and depth 
+###Asing the environmental temperature based on the month and depth
+###Here is the hourly temperature, attained from 21 days poriors to sampling date, they are differed by time, and depth 
+  mutate(Tk = ifelse(month == "May" & depth == 5,Temp_AprMay_soil5cm,  
                      ifelse (month == "May" & depth == 510, Temp_AprMay_soil10cm, 
                              ifelse(month == "September" & depth == 5, Temp_AugSep_soil5cm, 
-                                  Temp_AugSep_soil10cm)))) %>% ###Here is the hourly temperature, attained from 21 days poriors to sampling date, they are differed by time, and depth 
+                                  Temp_AugSep_soil10cm)))) %>% 
   mutate(Metabolic_Jhm2 = (exp(lni0))*(Biomass_mgm2^s)*(exp(-E_evK/(8.62*10^(-5)*Tk)))) %>%
    select(Sample_ID,year, month, plot, depth, order, suborder, family, Abundance,Density_m2, bodylength_mm, DW_ug, FW_ug, Biomass_mgm2, Metabolic_Jhm2)%>%
   ###this the average of individual biomass and biomass (both dry and fresh) of each family in each plot 
